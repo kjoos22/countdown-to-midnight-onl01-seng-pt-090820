490405395e9d1f1b2e7bdf42aa45db497b3ca1ba
Kristian Joos <kjoos22@gmail.com>
2020-09-12 16:23:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,7 +11,7 @@ end
 def countdown_with_sleep(starting_num)
   while starting_num > -1
     puts "#{starting_num} SECOND(S)!"
-    starting_num -= -1
+    starting_num -= 1
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
